bbfc8c67322bbc11bc8ffbe7a8541f13256ccbc2
VictorMiura <psnps4victor@gmail.com>
2024-04-06 17:39:35 -0300

    init:

---
+++ src/test/java/com/miura/twittersimplified/TwitterSimplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miura.twittersimplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterSimplifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
